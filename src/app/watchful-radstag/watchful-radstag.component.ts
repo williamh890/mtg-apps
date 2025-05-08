5cac753c2db31b6a7b0da34ff8e49abd312bca3a
William Horn <wbhorn@alaska.edu>
2025-05-07 22:17:38 -0800

    Add the stag!

--- src/app/watchful-radstag/watchful-radstag.component.ts
+++ src/app/watchful-radstag/watchful-radstag.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-watchful-radstag',
   templateUrl: './watchful-radstag.component.html',
   styleUrls: ['./watchful-radstag.component.scss'],
-  standalone: true
+  standalone: true,
+  imports: [
+    CommonModule,
+  ]
 })
 export class WatchfulRadstagComponent implements OnInit {
+  imageHeight = 0;
+  imageWidth = 0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @ViewChild('radstagImage') cardImage!: ElementRef;
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    console.log('resizing', this.cardImage);
+    if(!this.cardImage) {
+      return;
+    }
+
+    const width = this.cardImage.nativeElement.offsetWidth;
+    const height = this.cardImage.nativeElement.offsetHeight;
+
+    this.imageHeight = height;
+    this.imageWidth = width;
+  }
 }
